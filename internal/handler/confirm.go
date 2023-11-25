444cf2bc26a8c31357d2f257116921be703ae240
Maria <zaush@bk.ru>
2023-11-25 20:02:57 +0300

    added confirm

---
+++ internal/handler/confirm.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"errors"
+	"net/http"
+
+	api_errors "github.com/ex-rate/auth-service/internal/errors"
+	schema "github.com/ex-rate/auth-service/internal/schemas"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *handler) Confirm(ctx *gin.Context) {
+	var user schema.Registration
+	if err := ctx.BindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "invalid JSON", "err": err.Error()})
+		return
+	}
+
+	// проверка кода
+
+	c := ctx.Request.Context()
+
+	token, err := h.registration.RegisterUser(c, user)
+	if err != nil {
+		if errors.Is(err, api_errors.ErrUsernameAlreadyExists) || errors.Is(err, api_errors.ErrEmailAlreadyExists) ||
+			errors.Is(err, api_errors.ErrPhoneAlreadyExists) {
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "an error occured while creating user", "err": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "user successfully created", "token": token})
+}
