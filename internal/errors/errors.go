902b4af48fc7114056e42016d9fcfe9cef51c713
Maria <zaush@bk.ru>
2023-11-25 20:02:05 +0300

    added new errors

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -4,3 +4,7 @@ import "errors"
 
 var ErrUserNotExists = errors.New("user not exists")
 var ErrPasswordIncorrect = errors.New("password is incorrect")
+
+var ErrEmailAlreadyExists = errors.New("email already exists")
+var ErrPhoneAlreadyExists = errors.New("phone already exists")
+var ErrUsernameAlreadyExists = errors.New("username already exists")
